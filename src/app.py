2f237f506815f60bcf6ebea8edb42197d542ada4
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-04-07 18:57:26 +0200

    feat: Add initial Flask application and database setup

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from db import db
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.secret_key = os.urandom(24) 
+
+db.init_app(app)
+
+def main():
+    """This function initializes the database and starts the scheduler."""
+    with app.app_context():
+        db.create_all()
+
+if __name__ == '__main__':
+    main()
+    app.run(port=8082, debug=True)
