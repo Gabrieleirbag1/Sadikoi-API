5df0dbbacde1ac7ea2fb5e5125d3f01ec425e0d9
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-04-11 21:48:08 +0200

    feat: add db method

--- src/db.py
+++ src/db.py
@@ -1,3 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def add_to_db(object):
+    try:
+        db.session.add(object)
+        db.session.commit()
+        return {"message": "Object added to the database successfully"}
+    except Exception as e:
+        db.session.rollback()
+        return {"message": "An error occurred while adding the object to the database", "error": True}
