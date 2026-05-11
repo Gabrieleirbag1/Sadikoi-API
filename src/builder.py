b749b8935b956786cec5f35b6793577e3586fdfb
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-05-11 14:32:05 +0200

    feat(builder): add response builders for user, group, and chat message

---
+++ src/builder.py
@@ -0,0 +1,29 @@
+from models import ChatMessageModel, GroupModel, UserModel
+
+def build_user_response(user: UserModel) -> dict:
+    return {
+        "id": user.id,
+        "email": user.email,
+        "username": user.username,
+        "date_created": user.date_created
+    }
+
+def build_group_response(group: GroupModel) -> dict:
+    return {
+        "id": group.id,
+        "name": group.name,
+        "description": group.description,
+        "users": [build_user_response(user) for user in group.users],
+        "date_created": group.date_created
+    }
+
+def build_groups_response(groups: list[GroupModel]) -> list[dict]:
+    return [build_group_response(group) for group in groups]
+
+def build_chat_message_response(message: ChatMessageModel) -> dict:
+    return {
+        "id": message.id,
+        "content": message.content,
+        "timestamp": message.timestamp.isoformat(),
+        "sender": build_user_response(message.user)
+    }
